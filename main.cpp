d44922bb8a14fa39e93f568b45792cdc5753f55b
MrProcessor <dom.mrow@gmail.com>
2020-05-27 12:04:52 +0200

    delete needless comments

--- main.cpp
+++ main.cpp
@@ -21,21 +21,21 @@ int main()
 	{
 		switch (menu_choice)
 		{
-			case 1: menu1(); break; //menu startowe (poczatkowe) 
-			case 2: menu2(); break; //wybor jezyka
-			case 3: menu3(); break; //pokaz biezace ustawienia gry
-			case 4: menu4(); break; //zmien ustawienia gry
-			case 5: menu5(); break; //ile trzeba kolejnych znakow wlasnych w linii do wygranej
-			case 6: menu6(); break; //okresl rozmiar planszy
-			case 7: menu7(); break; //wybor znaku gracza (czlowieka) 
-			case 8: menu8(); break; //kto zaczyna gre
-			case 9: menu9(); break; //czy pamietac poprzednie ruchy 
-			case 10: menu10(); break; //graj 	 
-			case 11: menu11(); break; //przerwij biezaca rozgrywke
-			case 12: menu12(); break; //nowa rozgrywka
-			case 13: menu13(); break; //rozgrywka zakonczona
-			case 14: menu14(); break; //opis gry
-			case 15: menu15(); return 0; //zakoncz program gry
+			case 1: menu1(); break;
+			case 2: menu2(); break;
+			case 3: menu3(); break;
+			case 4: menu4(); break;
+			case 5: menu5(); break;
+			case 6: menu6(); break;
+			case 7: menu7(); break;
+			case 8: menu8(); break;
+			case 9: menu9(); break;
+			case 10: menu10(); break; 
+			case 11: menu11(); break;
+			case 12: menu12(); break;
+			case 13: menu13(); break;
+			case 14: menu14(); break;
+			case 15: menu15(); return 0;
 		}
 	}
 }
